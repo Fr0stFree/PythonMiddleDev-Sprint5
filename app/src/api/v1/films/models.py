d6b21e8e4a7cb1f0372ca6dfa30bdf02f440d41d
fr0stfree <me@frostfree.ru>
2023-10-22 15:47:20 +0300

    init api endpoints

--- app/src/api/v1/films/models.py
+++ app/src/api/v1/films/models.py
@@ -1,6 +1,61 @@
+from typing import Self
+from uuid import UUID
+
 from pydantic import BaseModel
 
+from films.models import Film, Genre, Person
 
-class Film(BaseModel):
-    id: str
+
+class DetailedFilm(BaseModel):
+    uuid: UUID
     title: str
+    imdb_rating: float
+    description: str
+    genre: list[str]
+    actors: list[str]
+    writers: list[str]
+    directors: list[str]
+
+    @classmethod
+    def from_elastic_schema(cls, film: Film) -> Self:
+        return cls(
+            genre=[genre.name for genre in film.genre],
+            actors=[actor.full_name for actor in film.actors],
+            writers=[writer.full_name for writer in film.writers],
+            directors=[director.full_name for director in film.directors],
+            **film.model_dump(exclude={"genre", "actors", "writers", "directors"}),
+        )
+
+
+class ShortenedFilm(BaseModel):
+    uuid: UUID
+    title: str
+    imdb_rating: float
+
+    @classmethod
+    def from_elastic_schema(cls, film: Film) -> Self:
+        return cls(**film.model_dump(exclude={"description", "genre", "actors", "writers", "directors"}))
+
+
+class DetailedGenre(BaseModel):
+    uuid: UUID
+    name: str
+
+    @classmethod
+    def from_elastic_schema(cls, genre: Genre) -> Self:
+        return cls(**genre.model_dump())
+
+
+class _PersonRoles(BaseModel):
+    uuid: UUID
+    roles: list[str]
+
+
+class DetailedPerson(BaseModel):
+    uuid: UUID
+    full_name: str
+    films: list[_PersonRoles]
+
+    @classmethod
+    def from_elastic_schema(cls, person: Person) -> Self:
+        return cls(**person.model_dump())
--- app/src/api/v1/films/router.py
+++ app/src/api/v1/films/router.py
@@ -1,17 +1,84 @@
-from http import HTTPStatus
+from uuid import UUID
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Path, Query, status
 
-from films.services import FilmService
-from .models import Film
+from films.services import FilmService, GenreService, PersonService
 
-router = APIRouter(prefix="/films")
+from .models import DetailedFilm, DetailedGenre, DetailedPerson, ShortenedFilm
+
+router = APIRouter()
 
 
-@router.get("/{film_id}")
-async def film_details(film_id: str, film_service: FilmService = Depends(FilmService.get_instance)) -> Film:
+@router.get("/films/{film_id}")
+async def film_details(
+    film_id: UUID = Path(...), film_service: FilmService = Depends(FilmService.get_instance)
+) -> DetailedFilm:
     film = await film_service.get_by_id(film_id)
     if not film:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="film not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Film not found")
 
-    return Film(id=film.id, title=film.title)
+    return DetailedFilm.from_elastic_schema(film)
+
+
+@router.get("/films")
+async def film_list(
+    search: str = Query(None),
+    sort: str = Query(None),
+    film_service: FilmService = Depends(FilmService.get_instance),
+) -> list[ShortenedFilm]:
+    films = await film_service.get_many(sort_params=sort, search_params=search)
+    return [ShortenedFilm.from_elastic_schema(film) for film in films]
+
+
+@router.get("/genres")
+async def genre_list(genre_service: GenreService = Depends(GenreService.get_instance)) -> list[DetailedGenre]:
+    genres = await genre_service.get_many()
+    return [DetailedGenre.from_elastic_schema(genre) for genre in genres]
+
+
+@router.get("/genres/{genre_id}")
+async def genre_details(
+    genre_id: UUID = Path(...), genre_service: GenreService = Depends(GenreService.get_instance)
+) -> DetailedGenre:
+    genre = await genre_service.get_by_id(genre_id)
+    if not genre:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Genre not found")
+
+    return DetailedGenre.from_elastic_schema(genre)
+
+
+@router.get("/persons")
+async def person_list(
+    search: str = Query(None),
+    sort: str = Query(None),
+    person_service: PersonService = Depends(PersonService.get_instance),
+) -> list[DetailedPerson]:
+    persons = await person_service.get_many(sort_params=sort, search_params=search)
+    return [DetailedPerson.from_elastic_schema(person) for person in persons]
+
+
+@router.get("/persons/{person_id}")
+async def person_details(
+    person_id: UUID = Path(...), person_service: PersonService = Depends(PersonService.get_instance)
+) -> DetailedPerson:
+    person = await person_service.get_by_id(person_id)
+    if not person:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Person not found")
+
+    return DetailedPerson.from_elastic_schema(person)
+
+
+@router.get("/persons/{person_id}/films")
+async def person_films(
+    person_id: UUID = Path(...),
+    person_service: PersonService = Depends(PersonService.get_instance),
+    film_service: FilmService = Depends(FilmService.get_instance),
+) -> list[ShortenedFilm]:
+    person = await person_service.get_by_id(person_id)
+    if not person:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Person not found")
+
+    films = await film_service.get_many(
+        search_params=f"actors:{person_id} OR writers:{person_id} OR directors:{person_id}"
+    )
+    return [ShortenedFilm.from_elastic_schema(film) for film in films]
